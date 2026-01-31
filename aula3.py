a545094958b14c20f1078377d2b640aa0b7f2a30
Kaio Willamis <gsasuke642@gmail.com>
2026-01-31 13:08:45 -0300

    Initial commit

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+
+# Aspas Simples
+print('Luiz Otavio')
+print('Luiz "Otavio"')
+
+# Aspas Duplas
+print("Luiz Otavio")
+print("Luiz 'Otavio'")
+
+#Escape
+print("Luiz \"Otavio")
+
+
