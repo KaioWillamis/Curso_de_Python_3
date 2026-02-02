974d245795ade9407a4b942a30b00c3ed06987e2
Kaio Willamis <gsasuke642@gmail.com>
2026-02-02 19:35:42 -0300

    atualizei

---
+++ aula5.py
@@ -0,0 +1,15 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa
+# só existem duas respostas possíveis
+# sim (true) ou não (false)
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que
+# questionar se um valor é igual a outro
+print(10==10) # Sim => true (Verdadeirio)
+print(10 == 11) # Não => False (Falso)
+
+print(type(True))
+print(type(False))
+
+print(type(10 == 10))
+print(type(10 == 11))
---
+++ aula7.py
@@ -0,0 +1,12 @@
+# Variáveis são usadas para salvar algo na mémoria do computador
+# PEP8: inicie variavéis com letras minúsculas, pode usar
+# números e underline_.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome(variável).
+# Uso: nome_variavel = expressão
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome: ', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
