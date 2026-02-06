22fab0ed99c671fa2ee1999e1c5f991fc1178459
Kaio Willamis <gsasuke642@gmail.com>
2026-02-06 19:31:50 -0300

    atualizei

---
+++ aula12.py
@@ -0,0 +1,10 @@
+nome = 'Kaio'
+altura = 1.76
+peso = 84.5
+imc = peso / (altura ** 2)
+print(nome, 'tem ', altura ,'de altura',)
+print('pesa ', peso, 'quilos e seu imc é', imc)
+
+# Luiz Otávio tem 1.75 de altura,
+# pesa 85 quilos e seu IMC é 
+# 29.320987435345
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = "Kaio"
+sobrenome = "Willamis"
+idade = 19
+ano_nascimento = "12/05/2006"
+maior_idade = idade >= 18
+altura_metros = 1.76
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade:', maior_idade)
+print('Altura em metros:', altura_metros)
